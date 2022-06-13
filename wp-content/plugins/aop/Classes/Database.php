596b219f89003d0a0401f36792883d95c6fb31a6
Np-Ng67 <92463049+Np-Ng67@users.noreply.github.com>
2022-06-13 13:38:02 +0200

    Add directory classes and database file

---
+++ wp-content/plugins/aop/Classes/Database.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace aop\Classes;
+
+use aop\Taxonomy\SpeciesTaxonomy;
+
+class Database {
+
+/**
+     * generateTables
+     *
+     * Creates the custom mysql tables for aop
+     * 
+     * @return void
+     */
+    static public function generateTables()
+    {
+
+        global $wpdb;
+
+        $tableName = $wpdb->prefix . 'pets';
+
+        $charsetCollate = $wpdb->get_charset_collate();
+
+        // écrire la requête à exécuter
+        // on crée une table (si elle n'existe pas déjà => IF NOT EXSITS), avec les champs suivants :
+        // developer_id => post de cpt developer
+        // technology_id => term de la taxo technology
+        // grade => note (niveau de maîtrise)
+        $sql = "
+            CREATE TABLE IF NOT EXISTS `{$tableName}` (
+                `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                `breed` VARCHAR(100) UNSIGNED,
+                `name` VARCHAR(100) UNSIGNED,
+                `birth_date` DATE,
+                `picture`,
+                `color` VARCHAR(100) UNSIGNED,
+                `size` BIGINT(20) UNSIGNED,
+                `weight` BIGINT(20) UNSIGNED,
+                `identification`
+                `description` 
+                PRIMARY KEY(`species_id`)
+            ) {$charsetCollate};
+        ";
+
+        // on utlise l'objet wpdb pour faire la requête
+        $wpdb->query($sql);
+    }
+
+    static public function dropTable()
+    {
+        // on récupère l'objet $wpdb => une globale déclarée par WP
+        global $wpdb;
+
+        // définir le nom de la table custom, on n'oublie pas d'utiliser le préfixe défini dans wp_config pour dynamiser le nom de la table
+        $tableName = $wpdb->prefix . 'developer_technology';
+
+        $wpdb->query('DROP TABLE IF EXISTS ' . $tableName);
+    }
+
+
+    static public function getTechnologiesByDeveloperId($developerPostId)
+    {
+        // on récupère l'objet wpdb qui permet d'interagir avec la BDD dans WP.
+        global $wpdb;
+
+        // on recompose le nom de la table en utilisant le préfixe
+        $tableName = $wpdb->prefix . 'developer_technology';
+
+        // réaliser la requête en bdd pour récupérer la liste des technologies associées au développeur $developerPostId
+        // on utilise la syntaxe sprintf() qui permet de définir des zones variables dans une chaîne => %d sera un integer
+        $sql = "SELECT * FROM `{$tableName}` WHERE developer_id=%d";
+
+        // on utilise wpdb:->prepare() pour préparer notre requête SQL et lui fournir les arguments à utiliser
+        $preparedQuery = $wpdb->prepare($sql, [$developerPostId]);
+
+        // exécution de la requête
+        // on récupère le résultat sous la forme d'un array associatif
+        $relationList = $wpdb->get_results($preparedQuery, ARRAY_A);
+        
+        
+        // on retourne un array qui contient pour chaque technologie
+        // - l'objet WP_Term 
+        // - le niveau de maîtrise (int)
+        $technologyList = [];
+        foreach ($relationList as $relation) {
+            $technology = [
+                'term' => get_term($relation['technology_id'], TechnologyTaxonomy::TAXONOMY_KEY),
+                'grade' => $relation['grade']
+            ];
+
+            $technologyList[$technology['term']->slug] = $technology;
+        }
+        
+        return $technologyList;
+    }
+
+    /**
+     * update the list of technologies associated to a specified developer, and the grade for this technology
+     *
+     * @param Array // technologies represented by term_id => grade
+     * @return void
+     */
+    static public function updateTechnologiesByDeveloperId($technologies, $developerPostId)
+    {
+        global $wpdb;
+        $tableName = $wpdb->prefix . 'developer_technology';
+        // Chaque technologie est un ensemble clé/valeur tel que term_id => grade
+        // Dans notre cas, on décide qu'une valeur de 0 pour grade veut dire que la technologie n'est pas associée. 
+        // on va d'abord supprimer toutes les technologies pour un développeur
+        // wpdb::delete() est fait pour
+        // le deuxième argument permet de donner un ensemble de WHERE
+        // Dans mon cas, WHERE developer_id=$developerPostId
+        $wpdb->delete($tableName, ['developer_id' => $developerPostId]);
+        // puis on ajoute des relations pour  chaque technologie qui a une note > 0
+        foreach ($technologies as $technologyId => $grade) {
+            if ($grade > 0) {
+                self::addTechnologyToDeveloper($technologyId, $grade, $developerPostId);
+            }
+        }
+    }
+
+    /**
+     * Registers a relation between a developer attached post and a technology, and the associated grade
+     *
+     * @param [type] $technologyId
+     * @param [type] $grade
+     * @param [type] $developerPostId
+     * @return void
+     */
+    static public function addTechnologyToDeveloper($technologyId, $grade, $developerPostId)
+    {
+        global $wpdb;
+        $tableName = $wpdb->prefix . 'developer_technology';
+        $wpdb->insert($tableName, ['technology_id' => $technologyId, 'grade' => $grade, 'developer_id' => $developerPostId]);
+    }
+
+
+}
