384b89cdd976b090a75fff9b895ad06c482d4184
pnobecourt <paul.nobecourt@orange.fr>
2022-06-15 10:18:59 +0200

    jwt-auth plugin installed, config ok

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -57,6 +57,10 @@ define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 
+// JWT auth plugin config
+define('JWT_AUTH_SECRET_KEY', 'your-top-secret-key');
+define('JWT_AUTH_CORS_ENABLE', true);
+
 /**#@-*/
 
 /**
