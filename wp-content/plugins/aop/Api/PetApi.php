4a133ff789a8d1cc235c4ddbddfaef5bce9905b7
pnobecourt <paul.nobecourt@orange.fr>
2022-06-21 00:25:27 +0200

    WIP - GetAllPetsMetaDataByOwnerId

--- wp-content/plugins/aop/Api/PetApi.php
+++ wp-content/plugins/aop/Api/PetApi.php
@@ -17,15 +17,15 @@ class PetApi
         ]);
         register_rest_route('aop/v1', '/pet/(?P<id>[\d]+)', [
             'methods' => 'GET',
-            'callback' => [self::class, 'handleGetPet'],
+            'callback' => [self::class, 'handleGetPetById'],
         ]);
         register_rest_route('aop/v1', '/pet/(?P<id>[\d]+)', [
             'methods' => 'POST',
-            'callback' => [self::class, 'handleEditPet'],
+            'callback' => [self::class, 'handleEditPetById'],
         ]);
         register_rest_route('aop/v1', '/pet/(?P<id>[\d]+)', [
             'methods' => 'DELETE',
-            'callback' => [self::class, 'handleDeletePet'],
+            'callback' => [self::class, 'handleDeletePetById'],
         ]);
     }
 
@@ -60,11 +60,11 @@ class PetApi
         return $response;
     }
 
-    public static function handleGetPet($request)
+    public static function handleGetPetById($request)
     {
         $petPostData = get_post($request['id'], ARRAY_A);
         
-        $petMetaData = PetDatabase::GetPetMetaData($request['id']);
+        $petMetaData = PetDatabase::GetPetMetaDataByPetId($request['id']);
 
         $preparedData = [
             'ID' => $petPostData['ID'],
@@ -81,7 +81,28 @@ class PetApi
         return $preparedData;
     }
 
-    public static function handleEditPet($request)
+    public static function handleGetAllPetsByOwnerId($request)
+    {
+        $petPostData = get_post($request['id'], ARRAY_A);
+        
+        $petMetaData = PetDatabase::GetAllPetsMetaDataByOwnerId($request['post_author']);
+
+        $preparedData = [
+            'ID' => $petPostData['ID'],
+            'content' => $petPostData['post_content'],
+            'breed' => $petMetaData[0]['breed'],
+            'identification' => $petMetaData[0]['identification'],
+            'birth_date' => $petMetaData[0]['birth_date'],
+            'color' => $petMetaData[0]['color'],
+            'size' => $petMetaData[0]['size'],
+            'weight' => $petMetaData[0]['weight']
+        ];
+
+        // on renvoie la réponse au format JSON
+        return $preparedData;
+    }
+
+    public static function handleEditPetById($request)
     {
         // cette méthode doit return quelque chose pour servir de réponse
         // selon la configuration de PHP, et lorsque le body de la requête est au format json, $_POST reste vide
@@ -104,7 +125,7 @@ class PetApi
         // si on a reçu un int
         if (is_int($petPostIdOrError)) {
 
-            PetDatabase::UpdatePetMetaData($petData, $petPostIdOrError);
+            PetDatabase::UpdatePetMetaDataByPetId($petData, $petPostIdOrError);
 
             $response = ["userId" => $petPostIdOrError];
         } else {
@@ -116,7 +137,7 @@ class PetApi
         return $response;
     }
     
-    public static function handleDeletePet($request)
+    public static function handleDeletePetById($request)
     {
         $userIdToDelete = (int) $request['id'];
         $userLoginToDelete = get_userdata($userIdToDelete)->data->user_login;
@@ -124,6 +145,9 @@ class PetApi
         include_once ABSPATH . 'wp-admin/includes/user.php';
 
         if (is_int($userIdToDelete) && $userIdToDelete > 1) {
+
+            PetDatabase::DeletePetMetaDataByPetId($request['id']);
+
             $response = wp_delete_user($userIdToDelete);
         } elseif (is_int($userIdToDelete) && $userIdToDelete == 1) {
             $response = "Impossible de supprimer le super-utilisateur";
--- wp-content/plugins/aop/Classes/PetDatabase.php
+++ wp-content/plugins/aop/Classes/PetDatabase.php
@@ -32,20 +32,31 @@ class PetDatabase
         );
     }
 
-    public static function GetPetMetaData($request)
+    public static function GetPetMetaDataByPetId($request)
     {
         
         global $wpdb;
         $tableName = $wpdb->prefix . 'pets';
-        $sql1 = "SELECT * FROM {$tableName} WHERE post_id = {$request}";
-        $sql2 = "SELECT * FROM aop_pets WHERE post_id = 28";
+        $sql = "SELECT * FROM {$tableName} WHERE post_id = {$request}";
         
-        $postMeta = $wpdb->get_results($sql1, ARRAY_A);
+        $postMeta = $wpdb->get_results($sql, ARRAY_A);
 
         return $postMeta;
     }
 
-    public static function UpdatePetMetaData($petData, $petPostId)
+    public static function GetAllPetsMetaDataByOwnerId($request)
+    {
+        
+        global $wpdb;
+        $tableName = $wpdb->prefix . 'pets';
+        $sql = "SELECT * FROM {$tableName} WHERE post_id = {$request}";
+        
+        $postMeta = $wpdb->get_results($sql, ARRAY_A);
+
+        return $postMeta;
+    }
+
+    public static function UpdatePetMetaDataByPetId($petData, $petPostId)
     {
 
         /* print_r("youpi!");
@@ -81,71 +92,18 @@ class PetDatabase
         );
     }
 
-    public static function handleEditPet($request)
+    public static function DeletePetMetaDataByPetId($request)
     {
-        // cette méthode doit return quelque chose pour servir de réponse
-        // selon la configuration de PHP, et lorsque le body de la requête est au format json, $_POST reste vide
-        // alternative : file_get_contents("php://input") qui permet de récupérer tout ce qui a été envoyé dans le body par la requête
-        // on récupère une string => attention, il faut la convertir en la décodant avec json_decode()
-        // on récupère les données sour forme de tableau associatif si le deuxième argument vaut true
-        $postData = json_decode(file_get_contents("php://input"), true);
-
-        // dans $postData on a un array associatif qui correspond à l'objet passé dans la requête ajax de userService.registerUser() côté VueJS
-        // en cas de succès de wp_create_user() on récupère l'id de l'utilisateur créés
-        // en cas d'échec un objet WP_Error
-        $userIdOrErrorObject = wp_insert_user([
-            'ID' => $request['id'],
-            'username' => $request['username'],
-            'user_login' => $request['user_login'],
-            'user_pass' => $request['user_pass'],
-            'user_nicename' => $request['user_nicename'],
-            'user_email' => $request['user_email'],
-            'display_name' => $request['display_name'],
-            'nickname' => $request['nickname'],
-            'first_name' => $request['first_name'],
-            'last_name' => $request['last_name'],
-            'user_url' => $request['user_url'],
-            'description' => $request['description'],
-            'show_admin_bar_front' => $request['show_admin_bar_front'],
-            'role' => $request['role'],
-            'locale' => $request['locale'],
-            'meta_input' => [
-                'country' => $request['country'],
-                'zip' => $request['zip'],
-                'city' => $request['city'],
-                'address' => $request['address'],
-                'phone' => $request['phone'],
-            ]
-        ]);
-
-        // si on a reçu un int
-        if (is_int($userIdOrErrorObject)) {
-            $response = ["userId" => $userIdOrErrorObject];
-        } else {
-            // sinon c'est une erreur
-            $response = $userIdOrErrorObject;
-        }
-
-        // on renvoie la réponse au format JSON
-        return $response;
-    }
-    
-    public static function handleDeletePet($request)
-    {
-        $userIdToDelete = (int) $request['id'];
-        $userLoginToDelete = get_userdata($userIdToDelete)->data->user_login;
-
-        include_once ABSPATH . 'wp-admin/includes/user.php';
-
-        if (is_int($userIdToDelete) && $userIdToDelete > 1) {
-            $response = wp_delete_user($userIdToDelete);
-        } elseif (is_int($userIdToDelete) && $userIdToDelete == 1) {
-            $response = "Impossible de supprimer le super-utilisateur";
-        } else {
-            $response = "Erreur lors de la suppression de l'utilisateur " . $userLoginToDelete;
-        }
         
-        // on renvoie la réponse au format JSON
-        return $response;
+        global $wpdb;
+        $tableName = $wpdb->prefix . 'pets';
+        $DeletedRowsOrError = $wpdb->delete(
+            $tableName,
+            ['post_id' => $request],
+            ['%d']
+        );
+
+        return $DeletedRowsOrError;
     }
+
 }
